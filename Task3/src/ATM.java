8f9448a85810cf6e44301e048d6902d48de68214
Sandesh Khatiwada <Khatiwadasandesh501@gmail.com>
2025-07-05 10:22:45 +0545

    Task 3 processing

--- Task3/src/ATM.java
+++ Task3/src/ATM.java
@@ -4,7 +4,7 @@ public class ATM implements ATMInterface{
 
     //Common for withdraw, deposit and checking balance
     public float amount(){
-        System.out.println("Enter the Amount");
+        System.out.print("Enter the Amount: ");
         Scanner sc = new Scanner(System.in);
         float amount = sc.nextFloat();
         return amount;
@@ -13,19 +13,35 @@ public class ATM implements ATMInterface{
     @Override
     public void withdrawing(float withdrawAmount) {
         System.out.println("Withdrawing amount....");
-        Bank.amountAfterWithdrawing(withdrawAmount);
+        if(Bank.amount>=withdrawAmount) {
+            Bank.amountAfterWithdrawing(withdrawAmount);
+            System.out.println("Withdraw success, new amount " + Bank.checkAmount());
+        }
+        else {
+            System.out.println("Withdraw Failed");
+            System.out.println("Your balance           "+ Bank.amount);
+            System.out.println("Your withdraw Request: "+ withdrawAmount);
+        }
+        randomFormattedSeperator();
     }
 
     @Override
     public void deposit(float depositAmount) {
         System.out.println("Depositing amount....");
         Bank.amountAfterDeposit(depositAmount);
+        System.out.println("Deposit success, new amount "+ Bank.checkAmount());
+        randomFormattedSeperator();
     }
 
     @Override
     public void checkingBalance() {
         System.out.println("Checking Balance....");
-        Bank.checkAmount();
+        float amnt = Bank.checkAmount();
+        System.out.println("Your Balance is "+amnt);
+        randomFormattedSeperator();
+    }
 
+    public static void randomFormattedSeperator(){
+        System.out.println("=========================");
     }
 }
--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -5,23 +5,29 @@ public class Main {
         System.out.println("Welcome to Sandesh's ATM");
 
         System.out.println("What operation would you like to perform?");
-        System.out.println("1. Withdraw\n2. Deposit\n3. Check Balance\n4. Exit");
-        Scanner sc = new Scanner(System.in);
-        int option = sc.nextInt();
-        ATM atm = new ATM();
+        do {
+            System.out.println("1. Withdraw\n2. Deposit\n3. Check Balance\n4. Exit\n");
+            Scanner sc = new Scanner(System.in);
+            int option = sc.nextInt();
+            ATM atm = new ATM();
 
-        switch (option){
-            case 1:
-                atm.withdrawing(atm.amount());
-                break;
-            case 2:
-                atm.deposit(atm.amount());
-                break;
-            case 3:
-                atm.checkingBalance();
-                break;
-            default:
-                System.out.println("Invalid Option");
-        }
+            switch (option) {
+                case 1:
+                    atm.withdrawing(atm.amount());
+                    break;
+                case 2:
+                    atm.deposit(atm.amount());
+                    break;
+                case 3:
+                    atm.checkingBalance();
+                    break;
+                case 4:
+                    System.out.println("Exiting....");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid Option");
+            }
+        }while (true);
     }
 }
