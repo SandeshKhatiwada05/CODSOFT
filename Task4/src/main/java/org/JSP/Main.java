522c9026f2c5358bc703909915a2e93572e1e388
Sandesh Khatiwada <Khatiwadasandesh501@gmail.com>
2025-07-07 18:03:07 +0545

    Task 4 processing

---
+++ Task4/src/main/java/org/JSP/Main.java
@@ -0,0 +1,28 @@
+package org.JSP;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        do{
+            System.out.println("Choose an option");
+            System.out.println("\n1. See lists of currency codes\n2. Currency Exchange\n3. Exit");
+            Scanner sc = new Scanner(System.in);
+            int chooser = sc.nextInt();
+
+            switch (chooser){
+                case 1:
+                    CurrencyLists.lists();
+                    break;
+                case 2:
+                    new CurrencyConverter();
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid Option");
+            }
+
+        }while (true);
+    }
+}
